36d645cac3064175c7131c1aa784c07309b052af
joseph <joseph.tran.goldenowl@gmail.com>
2025-10-26 21:56:49 +0700

    setup gateway user

--- user_service/django_template/settings/base.py
+++ user_service/django_template/settings/base.py
@@ -189,7 +189,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/users/static/"
 
 # Swagger
 EMAIL_CONTACT = "contact@snippets.local"
